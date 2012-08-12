a8bf7b11409e4b9d744c7e87a696efa6a7f95fce
David Silver <dsilver@candidatemetrics.com>
2012-08-12 09:24:22 -0700

    fix for www subdomain

--- blogs/config/routes.rb
+++ blogs/config/routes.rb
@@ -1,6 +1,6 @@
 Blogs::Application.routes.draw do
   resources :blogs
   resources :articles
-  match '', to: 'blogs#show', constraints: {subdomain: /.+/}
+  match '', to: 'blogs#show', constraints: lambda { |r| r.subdomain.present? && r.subdomain != 'www' }
   root to: 'blogs#index'
 end
